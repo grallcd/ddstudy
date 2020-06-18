f4b0bcc4bb6f15834a24a72667b897e53dd733c0
GRain-long <945812281@qq.com>
2020-06-18 23:27:26 +0800

    test

---
+++ ddstudy-demo/src/test/java/com/ddstudy/DemoTest.java
@@ -0,0 +1,17 @@
+package com.ddstudy;
+
+import org.junit.Test;
+
+/**
+ * @Classname DemoTest
+ * @Description TODO
+ * @Date 2020/6/18
+ * @Author Grain Rain
+ */
+public class DemoTest {
+
+    @Test
+    public void testDemo(){
+        System.out.println("Hello World");
+    }
+}
