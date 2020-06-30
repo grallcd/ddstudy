b42f4bc604ca930ecdd8ded0a5e405c381b51c7c
GRain-long <945812281@qq.com>
2020-06-30 18:29:55 +0800

    modify enum

--- ddstudy-designpattern/src/test/java/com/ddstudy/builder/BuilderTest.java
+++ ddstudy-designpattern/src/test/java/com/ddstudy/builder/BuilderTest.java
@@ -14,16 +14,16 @@ public class BuilderTest {
     @Test
     void testBuilder() {
         Person java = new Person
-                .Builder("Java", Gender.MALE)
+                .Builder("Java", GenderEnum.MALE)
                 .withDescription("Java is the best language")
                 .build();
 
-        Person python = new Person.Builder("Python", Gender.FEMALE).build();
+        Person python = new Person.Builder("Python", GenderEnum.FEMALE).build();
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new Person.Builder(null, Gender.MALE).build());
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Person.Builder(null, GenderEnum.MALE).build());
         Assertions.assertThrows(IllegalArgumentException.class, () -> new Person.Builder("PHP", null).build());
 
-        Assertions.assertEquals(Gender.MALE, java.getGender());
+        Assertions.assertEquals(GenderEnum.MALE, java.getGender());
         Assertions.assertEquals("Python", python.getName());
 
 
