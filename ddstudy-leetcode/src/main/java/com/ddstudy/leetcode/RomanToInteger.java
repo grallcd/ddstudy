2b8a1627b162badd108d941eb999ff5d69c2d8ab
longliang <945812281@qq.com>
2020-12-03 12:28:43 +0800

    feat：新增罗马数字转整数

---
+++ ddstudy-leetcode/src/main/java/com/ddstudy/leetcode/RomanToInteger.java
@@ -0,0 +1,112 @@
+package com.ddstudy.leetcode;
+
+/**
+ * 罗马数字包含以下七种字符: I， V， X， L，C，D 和 M。
+ * <p>
+ * 字符          数值
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ * 例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+ * <p>
+ * 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，
+ * 所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+ * <p>
+ * I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+ * X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+ * C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+ * 给定一个罗马数字，将其转换成整数。输入确保在 1 到 3999 的范围内。
+ * <p>
+ * 示例 1:
+ * 输入: "III"
+ * 输出: 3
+ * <p>
+ * 示例 2:
+ * 输入: "IV"
+ * 输出: 4
+ * <p>
+ * 示例 3:
+ * 输入: "IX"
+ * 输出: 9
+ * <p>
+ * 示例 4:
+ * 输入: "LVIII"
+ * 输出: 58
+ * 解释: L = 50, V= 5, III = 3.
+ * <p>
+ * 示例 5:
+ * 输入: "MCMXCIV"
+ * 输出: 1994
+ * 解释: M = 1000, CM = 900, XC = 90, IV = 4.
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/roman-to-integer
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Grain Rain
+ * @date 2020/12/3
+ */
+public class RomanToInteger {
+
+    /**
+     * 罗马数字由 I,V,X,L,C,D,M 构成；
+     * 当小值在大值的左边，则减小值，如 IV=5-1=4；
+     * 当小值在大值的右边，则加小值，如 VI=5+1=6；
+     * 由上可知，右值永远为正，因此最后一位必然为正。
+     *
+     * @param s
+     * @return
+     */
+    public int romanToInt(String s) {
+
+        char[] chars = s.toCharArray();
+        int result = 0;
+        int preNum = charToInt(chars[0]);
+        for (int i = 1; i < chars.length; i++) {
+            int cur = charToInt(chars[i]);
+
+            if (preNum < cur) {
+                result -= preNum;
+            } else {
+                result += preNum;
+            }
+            preNum = cur;
+        }
+        result += preNum;
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "MCMXCIV";
+        RomanToInteger romanToInteger = new RomanToInteger();
+        int result = romanToInteger.romanToInt(s);
+        System.out.println(result);
+    }
+
+
+    private int charToInt(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                throw new IllegalArgumentException("非法字符");
+        }
+    }
+}
