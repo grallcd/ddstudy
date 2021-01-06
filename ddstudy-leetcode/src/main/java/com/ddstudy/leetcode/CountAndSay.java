c0761a3c262722aa29809ef79020d35faf174d99
longliang <945812281@qq.com>
2021-01-06 16:03:49 +0800

    feat：新增外观数列

---
+++ ddstudy-leetcode/src/main/java/com/ddstudy/leetcode/CountAndSay.java
@@ -0,0 +1,68 @@
+package com.ddstudy.leetcode;
+
+/**
+ * 给定一个正整数 n ，输出外观数列的第 n 项。
+ * <p>
+ * 「外观数列」是一个整数序列，从数字 1 开始，序列中的每一项都是对前一项的描述。
+ * <p>
+ * 你可以将其视作是由递归公式定义的数字字符串序列：
+ * <p>
+ * countAndSay(1) = "1"
+ * countAndSay(n) 是对 countAndSay(n-1) 的描述，然后转换成另一个数字字符串。
+ * 前五项如下：
+ * <p>
+ * 1.     1
+ * 2.     11
+ * 3.     21
+ * 4.     1211
+ * 5.     111221
+ * 第一项是数字 1
+ * 描述前一项，这个数是 1 即 “ 一 个 1 ”，记作 "11"
+ * 描述前一项，这个数是 11 即 “ 二 个 1 ” ，记作 "21"
+ * 描述前一项，这个数是 21 即 “ 一 个 2 + 一 个 1 ” ，记作 "1211"
+ * 描述前一项，这个数是 1211 即 “ 一 个 1 + 一 个 2 + 二 个 1 ” ，记作 "111221"
+ * 要 描述 一个数字字符串，首先要将字符串分割为 最小 数量的组，每个组都由连续的最多 相同字符 组成。然后对于每个组，先描述字符的数量，然后描述字符，形成一个描述组。要将描述转换为数字字符串，先将每组中的字符数量用数字替换，再将所有描述组连接起来。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/count-and-say
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Grain Rain
+ * @date 2021/1/6
+ */
+public class CountAndSay {
+
+    public static void main(String[] args) {
+        int n = 5;
+        CountAndSay solution = new CountAndSay();
+        String s = solution.countAndSay(n);
+        System.out.println(s);
+    }
+
+
+    public String countAndSay(int n) {
+        // 递归终止条件
+        if (n == 1) {
+            return "1";
+        }
+        StringBuilder res = new StringBuilder();
+        // 拿到上一层的字符串
+        String str = countAndSay(n - 1);
+        int length = str.length();
+        // 开始指针为0
+        int start = 0;
+        // 注意这从起始条件要和下面长度统一
+        for (int i = 1; i < length + 1; i++) {
+            // 字符串最后一位直接拼接
+            if (i == length) {
+                res.append(i - start).append(str.charAt(start));
+                // 直到start位的字符串和i位的字符串不同，拼接并更新start位
+            } else if (str.charAt(i) != str.charAt(start)) {
+                res.append(i - start).append(str.charAt(start));
+                start = i;
+            }
+        }
+        return res.toString();
+    }
+
+}
