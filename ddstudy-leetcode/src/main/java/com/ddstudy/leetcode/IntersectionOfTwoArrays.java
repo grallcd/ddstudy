6b0da23cd682502dccd4d7cff494dd78545f2083
longliang <945812281@qq.com>
2021-02-07 09:18:54 +0800

    feat：新增两个数组的交集

---
+++ ddstudy-leetcode/src/main/java/com/ddstudy/leetcode/IntersectionOfTwoArrays.java
@@ -0,0 +1,62 @@
+package com.ddstudy.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出：[2]
+ * 示例 2：
+ * <p>
+ * 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出：[9,4]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/intersection-of-two-arrays
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Grain Rain
+ * @date 2021/2/7
+ */
+public class IntersectionOfTwoArrays {
+
+    public static void main(String[] args) {
+
+        IntersectionOfTwoArrays solution = new IntersectionOfTwoArrays();
+        int[] nums1 = {1, 2};
+
+        int[] nums2 = {2, 1};
+        int[] result = solution.intersection(nums1, nums2);
+        System.out.println(Arrays.toString(result));
+
+    }
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int length1 = nums1.length, length2 = nums2.length;
+        int[] intersection = new int[length1 + length2];
+        int index = 0, index1 = 0, index2 = 0;
+        while (index1 < length1 && index2 < length2) {
+            int num1 = nums1[index1], num2 = nums2[index2];
+            if (num1 == num2) {
+                // 保证加入元素的唯一性
+                if (index == 0 || num1 != intersection[index - 1]) {
+                    intersection[index++] = num1;
+                }
+                index1++;
+                index2++;
+            } else if (num1 < num2) {
+                index1++;
+            } else {
+                index2++;
+            }
+        }
+        return Arrays.copyOfRange(intersection, 0, index);
+    }
+
+}
