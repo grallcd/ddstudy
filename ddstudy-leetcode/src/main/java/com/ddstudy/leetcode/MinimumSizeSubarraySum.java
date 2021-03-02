c0b11ff77e192cc43e65c5a06de2137381d4bd42
longliang <945812281@qq.com>
2021-03-02 17:50:29 +0800

    feat：新增长度最小的子数组

---
+++ ddstudy-leetcode/src/main/java/com/ddstudy/leetcode/MinimumSizeSubarraySum.java
@@ -0,0 +1,79 @@
+package com.ddstudy.leetcode;
+
+/**
+ * 给定一个含有n个正整数的数组和一个正整数 target 。
+ * <p>
+ * 找出该数组中满足其和 ≥ target 的长度最小的 连续子数组[numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：target = 7, nums = [2,3,1,2,4,3]
+ * 输出：2
+ * 解释：子数组[4,3]是该条件下的长度最小的子数组。
+ * 示例 2：
+ * <p>
+ * 输入：target = 4, nums = [1,4,4]
+ * 输出：1
+ * 示例 3：
+ * <p>
+ * 输入：target = 11, nums = [1,1,1,1,1,1,1,1]
+ * 输出：0
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= target <= 109
+ * 1 <= nums.length <= 105
+ * 1 <= nums[i] <= 105
+ * <p>
+ * 进阶：
+ * <p>
+ * 如果你已经实现 O(n) 时间复杂度的解法, 请尝试设计一个 O(n log(n)) 时间复杂度的解法。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-size-subarray-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Grain Rain
+ * @date 2021/3/2
+ */
+public class MinimumSizeSubarraySum {
+
+    public static void main(String[] args) {
+
+        MinimumSizeSubarraySum solution = new MinimumSizeSubarraySum();
+        int target = 7;
+        int[] nums = {2, 3, 1, 2, 4, 3};
+        int i = solution.minSubArrayLen(target, nums);
+        System.out.println(i);
+
+    }
+
+    /**
+     * 滑动窗口
+     *
+     * @param target
+     * @param nums
+     * @return
+     */
+    public int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+
+        int ans = Integer.MAX_VALUE;
+        int start = 0, end = 0;
+        int sum = 0;
+        while (end < n) {
+            sum += nums[end];
+            while (sum >= target) {
+                ans = Math.min(ans, end - start + 1);
+                sum -= nums[start];
+                start++;
+            }
+            end++;
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+    }
+}
