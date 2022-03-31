1b50239f0ccff42745b7a134fbcb63c473c6619d
longliang <945812281@qq.com>
2022-03-31 15:53:12 +0800

    feat: jdk 动态代理原理

---
+++ app/src/main/java/com/ddstudy/app/a13/$Proxy0.java
@@ -0,0 +1,67 @@
+package com.ddstudy.app.a13;
+
+import com.ddstudy.app.a13.A13.Foo;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.lang.reflect.UndeclaredThrowableException;
+
+/**
+ * @author grallcd
+ * @since 2022/3/31
+ */
+public class $Proxy0 extends Proxy implements Foo {
+
+
+    private static final Method foo;
+
+    private static final Method bar;
+
+    static {
+        try {
+            foo = Foo.class.getMethod("foo");
+            bar = Foo.class.getMethod("bar");
+        } catch (NoSuchMethodException e) {
+            throw new NoSuchMethodError();
+        }
+    }
+
+    /**
+     * Constructs a new {@code Proxy} instance from a subclass
+     * (typically, a dynamic proxy class) with the specified value
+     * for its invocation handler.
+     *
+     * @param h the invocation handler for this proxy instance
+     * @throws NullPointerException if the given invocation handler, {@code h},
+     *                              is {@code null}.
+     */
+    protected $Proxy0(InvocationHandler h) {
+        super(h);
+    }
+
+
+    @Override
+    public void foo() {
+        try {
+            h.invoke(this, foo, new Object[0]);
+        } catch (RuntimeException | Error e) {
+            throw e;
+        } catch (Throwable e) {
+            throw new UndeclaredThrowableException(e);
+        }
+    }
+
+    @Override
+    public int bar() {
+        try {
+            return (int) h.invoke(this, bar, new Object[0]);
+        } catch (RuntimeException | Error e) {
+            throw e;
+        } catch (Throwable e) {
+            throw new UndeclaredThrowableException(e);
+        }
+    }
+
+}
+
