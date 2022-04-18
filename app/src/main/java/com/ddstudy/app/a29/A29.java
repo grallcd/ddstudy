240cf3fbb6f1d2cf5d4734b728682525fd117ade
longliang <945812281@qq.com>
2022-04-18 22:30:03 +0800

    feat: @ControllerAdvice - ResponseBodyAdvice

---
+++ app/src/main/java/com/ddstudy/app/a29/A29.java
@@ -0,0 +1,83 @@
+package com.ddstudy.app.a29;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.DefaultParameterNameDiscoverer;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.method.ControllerAdviceBean;
+import org.springframework.web.method.annotation.ExpressionValueMethodArgumentResolver;
+import org.springframework.web.method.annotation.RequestHeaderMethodArgumentResolver;
+import org.springframework.web.method.annotation.RequestParamMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolverComposite;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandlerComposite;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.servlet.mvc.method.annotation.*;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author grallcd
+ * @since 2022/4/18
+ */
+public class A29 {
+
+    public static void main(String[] args) throws Exception {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(WebConfig.class);
+
+        ServletInvocableHandlerMethod handlerMethod = new ServletInvocableHandlerMethod(
+                context.getBean(WebConfig.MyController.class),
+                WebConfig.MyController.class.getMethod("user")
+        );
+        handlerMethod.setDataBinderFactory(new ServletRequestDataBinderFactory(Collections.emptyList(), null));
+        handlerMethod.setParameterNameDiscoverer(new DefaultParameterNameDiscoverer());
+        handlerMethod.setHandlerMethodArgumentResolvers(getArgumentResolvers(context));
+        handlerMethod.setHandlerMethodReturnValueHandlers(getReturnValueHandlers(context));
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        ModelAndViewContainer container = new ModelAndViewContainer();
+        handlerMethod.invokeAndHandle(new ServletWebRequest(request, response), container);
+
+        System.out.println(new String(response.getContentAsByteArray(), StandardCharsets.UTF_8));
+    }
+
+    public static HandlerMethodReturnValueHandlerComposite getReturnValueHandlers(AnnotationConfigApplicationContext context) {
+        // 添加 advice
+        List<ControllerAdviceBean> annotatedBeans = ControllerAdviceBean.findAnnotatedBeans(context);
+        List<Object> collect = annotatedBeans.stream().filter(b -> ResponseBodyAdvice.class.isAssignableFrom(b.getBeanType()))
+                .collect(Collectors.toList());
+
+        HandlerMethodReturnValueHandlerComposite composite = new HandlerMethodReturnValueHandlerComposite();
+        composite.addHandler(new ModelAndViewMethodReturnValueHandler());
+        composite.addHandler(new ViewNameMethodReturnValueHandler());
+        composite.addHandler(new ServletModelAttributeMethodProcessor(false));
+        composite.addHandler(new HttpEntityMethodProcessor(Stream.of(new MappingJackson2HttpMessageConverter()).collect(Collectors.toList())));
+        composite.addHandler(new HttpHeadersReturnValueHandler());
+        composite.addHandler(new RequestResponseBodyMethodProcessor(Stream.of(new MappingJackson2HttpMessageConverter()).collect(Collectors.toList()), collect));
+        composite.addHandler(new ServletModelAttributeMethodProcessor(true));
+        return composite;
+    }
+
+    public static HandlerMethodArgumentResolverComposite getArgumentResolvers(AnnotationConfigApplicationContext context) {
+        HandlerMethodArgumentResolverComposite composite = new HandlerMethodArgumentResolverComposite();
+        composite.addResolvers(
+                new RequestParamMethodArgumentResolver(context.getDefaultListableBeanFactory(), false),
+                new PathVariableMethodArgumentResolver(),
+                new RequestHeaderMethodArgumentResolver(context.getDefaultListableBeanFactory()),
+                new ServletCookieValueMethodArgumentResolver(context.getDefaultListableBeanFactory()),
+                new ExpressionValueMethodArgumentResolver(context.getDefaultListableBeanFactory()),
+                new ServletRequestMethodArgumentResolver(),
+                new ServletModelAttributeMethodProcessor(false),
+                new RequestResponseBodyMethodProcessor(Stream.of(new MappingJackson2HttpMessageConverter()).collect(Collectors.toList())),
+                new ServletModelAttributeMethodProcessor(true),
+                new RequestParamMethodArgumentResolver(context.getDefaultListableBeanFactory(), true)
+        );
+        return composite;
+    }
+}
